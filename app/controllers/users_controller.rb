0628a71db447f91e9fe072c153d71f3afda692d0
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-22 17:33:55 -0500

    successfully set up email notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      redirect_to @user
-    else
-      render "new"
+
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        AlertMailer.welcome_email(@user).deliver_now
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   def edit
     @user = User.find(params[:id])
--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -4,7 +4,7 @@ class AlertMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = "http://localhost:3000/login"
-    mail(to: @user.email, subject: 'Welcome to the Site')
+    mail(to: @user.email, subject: 'Welcome to TopThat!')
   end
 
 
