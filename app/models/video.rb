26029daba54c2144b24a71af7f68ce2c8de1d197
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-22 08:37:33 -0500

    updated time-functionality, working on turning it into a helper

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,6 +6,7 @@ class Video < ActiveRecord::Base
   has_one :private_arena_as_challengee, class_name: "PrivateArena", foreign_key: :challengee_video_id
   has_one :public_arena_as_challenger, class_name: "PublicArena", foreign_key: :challenger_video_id
   has_one :public_arena_as_challengee, class_name: "PublicArena", foreign_key: :challengee_video_id
+  validates :title, presence: true
 
 
 mount_uploader :data_content, VideoUploader
