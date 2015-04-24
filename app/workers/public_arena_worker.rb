07fdce17a1ac70fde8e783da8dd5b616bebfe2e6
Hola! Yo soy Llama <jordanedwardobey@gmail.com>
2015-04-23 20:08:51 -0500

    removed sidekiq mailer and added deliver_now methods

--- app/workers/public_arena_worker.rb
+++ app/workers/public_arena_worker.rb
@@ -7,21 +7,21 @@ class PublicArenaWorker
     if arena.challenger_won?
       user = arena.challenger_video.user
       loser = arena.challengee_video.user
-      AlertMailer.winner_email(user, arena)
-      AlertMailer.loser_email(loser, arena)
+      AlertMailer.winner_email(user, arena).deliver_now
+      AlertMailer.loser_email(loser, arena).deliver_now
     elsif arena.challengee_won?
       user = arena.challengee_video.user
       loser = arena.challenger_video.user
-      AlertMailer.winner_email(user, arena)
-      AlertMailer.loser_email(loser, arena)
+      AlertMailer.winner_email(user, arena).deliver_now
+      AlertMailer.loser_email(loser, arena).deliver_now
     elsif arena.tie?
       user_one = arena.challengee_video.user
-      AlertMailer.tie_email(user_one, arena)
+      AlertMailer.tie_email(user_one, arena).deliver_now
       user_two = arena.challengee_video.user
-      AlertMailer.tie_email(user_two, arena)
+      AlertMailer.tie_email(user_two, arena).deliver_now
     else
       user = arena.challenger_video.user
-      AlertMailer.no_challengee_email(user, arena)
+      AlertMailer.no_challengee_email(user, arena).deliver_now
     end
   end
 
