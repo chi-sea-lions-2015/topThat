0b57e660627a5b975f53cfc9c22ed9cf4e9cf4ce
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-05-07 15:38:54 -0500

    voting limited, but buggy. method in user model and votes controller updated

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,18 +2,17 @@ class VotesController < ApplicationController
   def create
     if current_user
       @video = Video.find(params[:video_id])
-
-      if @video.public_arena_as_challenger
+      if User.voted?(current_user.id, @video.id) == false && @video.public_arena_as_challenger
         @vote = @video.votes.create(voter: current_user, video_id: params[:video_id])
         @public_arena = @video.public_arena_as_challenger
-      else
+      elsif User.voted?(current_user.id, @video.id) == false
         @vote = @video.votes.create(voter: current_user, video_id: params[:video_id])
         @public_arena = @video.public_arena_as_challengee
       end
       @challenger_video = @public_arena.challenger_video
       redirect_to video_public_arena_path(@challenger_video, @public_arena)
     else
-      
+      redirect_to login_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,13 +20,28 @@ class User < ActiveRecord::Base
 
 #write method to create friends
   def self.voted?(user_id, video_id)
-    video = Video.find(video_id)
-    user = User.find(user_id)
-    vote = video.votes.find_by(voter_id: user_id)
-      if vote  != nil 
-        true 
+    @video = Video.find(video_id)
+    @public_arena_as_challenger = @video.public_arena_as_challenger
+    @public_arena_as_challengee = @video.public_arena_as_challengee
+    if @public_arena_as_challenger != nil
+      @challengee_video = @public_arena_as_challenger.challengee_video
+      @challenger_vote = @video.votes.find_by(voter_id: user_id)
+      @challengee_vote = @challengee_video.votes.find_by(voter_id: user_id)
+      if @challenger_vote != nil && @challengee_vote !=nil
+        true
       else
-        false 
-      end 
-  end 
+        false
+      end
+    else
+      @challenger_video = @public_arena_as_challengee.challenger_video
+      @challenger_vote = @challenger_video.votes.find_by(voter_id: user_id)
+      @challengee_vote = video.votes.find_by(voter_id: user_id)
+      if @challenger_vote != nil && @challengee_vote !=nil
+        true
+      else
+        false
+      end
+    end
+  end
+
 end
