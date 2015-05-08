3d784c656bbc3abe717dd501ea3188f122f27415
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-05-08 12:21:05 -0500

    limiting voting per video and redirecting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,12 +6,13 @@ class VotesController < ApplicationController
       if @video.public_arena_as_challenger && User.voted?(current_user, @video) == false
         @vote = @video.votes.create(voter: current_user, video_id: params[:video_id])
         @public_arena = @video.public_arena_as_challenger
-      elsif @video.public_arena_as_challengee&& User.voted?(current_user, @video) == false
+        redirect_to video_public_arena_path(@video, @public_arena)
+      elsif @video.public_arena_as_challengee && User.voted?(current_user, @video) == false
         @vote = @video.votes.create(voter: current_user, video_id: params[:video_id])
         @public_arena = @video.public_arena_as_challengee
+        @challenger_video = @public_arena.challenger_video
+        redirect_to video_public_arena_path(@challenger_video, @public_arena)
       end
-      @challenger_video = @public_arena.challenger_video
-      redirect_to video_public_arena_path(@challenger_video, @public_arena)
     else
       redirect_to login_path
     end
