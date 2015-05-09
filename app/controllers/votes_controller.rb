b38880d558072e440e41fbefaef38a4c52d0c354
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-05-09 15:07:00 -0500

    flash working only when visiting the page from somewhere else, not when staying on the page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -16,12 +16,12 @@ class VotesController < ApplicationController
         if @video.public_arena_as_challenger
           @public_arena = @video.public_arena_as_challenger
           puts "Hello!"
-          redirect_to video_public_arena_path(@video, @public_arena), notice: "You already voted!"
+          redirect_to video_public_arena_path(@video, @public_arena), notice: "You already voted on this battle!"
         else
           @public_arena = @video.public_arena_as_challengee
           @challenger_video = @public_arena.challenger_video
           puts "Hi!"
-          redirect_to video_public_arena_path(@challenger_video, @public_arena), notice: "You already voted!"
+          redirect_to video_public_arena_path(@challenger_video, @public_arena), notice: "You already voted on this battle!"
         end
       end
     else
