83f9fc0011c95221ce5ffac863fdf2c514bf50b5
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-05-09 13:39:21 -0500

    updating redirect if user voted

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,6 +12,17 @@ class VotesController < ApplicationController
         @public_arena = @video.public_arena_as_challengee
         @challenger_video = @public_arena.challenger_video
         redirect_to video_public_arena_path(@challenger_video, @public_arena)
+      else
+        if @video.public_arena_as_challenger
+          @public_arena = @video.public_arena_as_challenger
+          puts "Hello!"
+          redirect_to video_public_arena_path(@video, @public_arena)
+        else
+          @public_arena = @video.public_arena_as_challengee
+          @challenger_video = @public_arena.challenger_video
+          puts "Hi!"
+          redirect_to video_public_arena_path(@challenger_video, @public_arena)
+        end
       end
     else
       redirect_to login_path
