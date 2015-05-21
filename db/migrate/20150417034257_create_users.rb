30664943355f876ac01a577a6b13658bc15d1ad3
BackStreetSoy <jordanedwardobey@gmail.com>
2015-05-21 11:29:11 -0500

    no default

--- db/migrate/20150417034257_create_users.rb
+++ db/migrate/20150417034257_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username
       t.string :email
       t.string :avatar
-      t.string :about_me
       t.string :password_digest
 
       t.timestamps null: false
