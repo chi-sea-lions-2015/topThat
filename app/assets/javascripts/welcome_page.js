a3610ab4132152a1d30bba63b69a5aa7b1547365
BackStreetSoy <jordanedwardobey@gmail.com>
2015-05-27 14:06:15 -0500

    ajax linked to button

--- app/assets/javascripts/welcome_page.js
+++ app/assets/javascripts/welcome_page.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   console.log("what?")
 
-  $("#signup_link").on("click", function(e){
+  $("#signup_button").on("click", function(e){
       e.preventDefault();
       console.log("poop")
 
     $.ajax({
-      url: $(this).attr("href"),
-      method: $(this).attr("method"),
+      url: $("#signup_link").attr("href"),
+      method: $("#signup_link").attr("method"),
       success: function(response){
         var signup_form = $(response).find("#sign_up_form");
            $("#opening_forms").html(signup_form[0])
